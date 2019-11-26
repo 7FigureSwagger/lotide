5b227734724b8b37e2eaa0799cfdf4d23a69f61d
7 <swistek.labs@gmail.com>
2019-11-26 04:12:31 +0000

    d1

--- assertEqual.js
+++ assertEqual.js
@@ -4,9 +4,9 @@ const passed = 'Assertion Passed';
 
 const assertEqual = function(actual, expected) {
   if(actual != expected){
-    console.log('😔😔 Assertion failed, ' + actual + ' != ' + expected);
+    console.log(`😔😔 Assertion failed, ${actual} != ${expected}`);
   } else if(actual === expected) {
-    console.log('🧙‍♂️🧙‍♂️ Assertion passed ' + actual + ' == ' + expected);
+    console.log(`🧙‍♂️🧙‍♂️ Assertion passed ${actual} == ${expected}`);
   }
   //console.assert(actual === expected, {actual, passed});
 }
